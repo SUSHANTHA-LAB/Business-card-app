b0e4eaaa9c38a3988b36547f862dec1f4bbb4878
Sushantha Koonadi <sushantha42@gmail.com>
2020-09-27 23:21:23 +0530

    first commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -7,6 +7,46 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.red,Colors.pink]
+          )
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment : MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Colors.yellowAccent,
+                    child: CircleAvatar(
+                      radius: 55,
+                      backgroundColor: Colors.greenAccent,
+                    ),
+                  ),
+                SizedBox(
+                  height: 10,
+                ),
+                  Container(
+
+                    height: 50,
+
+                    child: Text('hello', textAlign: TextAlign.center, style: TextStyle(color: Colors.yellowAccent,fontSize: 50, fontFamily: 'BungeeShade'),),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
